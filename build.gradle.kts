2901d5e75ce4e4abb716cd2ffffb29ea48186212
Cerclepalace <durand.noa13@icloud.com>
2026-09-22 06:56:30 +0200

    build: add Paper 26.2 plugin foundation

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    java
+}
+
+group = "fr.cerclepalace"
+version = "0.1.0-SNAPSHOT"
+
+description = "Next-generation PvP Minecraft server foundation"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:26.2.build.96")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(25))
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+    options.release.set(25)
+}
+
+tasks.jar {
+    archiveBaseName.set("nexus-core")
+}
